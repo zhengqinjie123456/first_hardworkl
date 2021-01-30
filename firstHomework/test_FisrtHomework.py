c606284f95409fdb8cce6e153665ba168a12b9fd
zhengqj <850259582@qq.com>
2021-01-30 15:45:48 +0800

    上传

--- firstHomework/test_FisrtHomework.py
+++ firstHomework/test_FisrtHomework.py
@@ -8,7 +8,10 @@ def readYaml(path):
     with open(path) as f:
         result = yaml.safe_load(f)
     return result
-datas:dict=readYaml('./YamlDir/YamlData.yaml')
+
+
+datas: dict = readYaml('./YamlDir/YamlData.yaml')
+
 
 
 
@@ -19,16 +22,16 @@ class Test_calculate():
     def teardown(self):
         print("结束计算")
 
-    @pytest.mark.parametrize("firstNum,secNum,result",datas["add"])
-    def test_add(self,firstNum,secNum,result):
-        Sum=firstNum+secNum
+    @pytest.mark.parametrize("firstNum,secNum,result", datas["add"])
+    def test_add(self, firstNum, secNum, result):
+        Sum = firstNum + secNum
         NewSum = float(Decimal(Sum).quantize(Decimal("0.0001")))
-        assert  float(result)== NewSum
+        assert float(result) == NewSum
 
     @pytest.mark.parametrize("firstNum,secNum,result", datas["div"])
-    def test_div(self,firstNum,secNum,result):
+    def test_div(self, firstNum, secNum, result):
         try:
-            Div=firstNum/secNum
+            Div = firstNum / secNum
         except Exception as e:
             print(f"数值类型或者值有误：{e}")
         NewDiv = float(Decimal(Div).quantize(Decimal("0.0001")))
